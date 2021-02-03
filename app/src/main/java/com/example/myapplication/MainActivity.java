bdc93255ca611cd265c610710f70e52b9c195ce6
ZogopZ <zoiszogop@gmail.com>
2021-02-03 21:56:51 +0200

    to-do development v_0.03
    - Reworked application's main layout to grid.
    - Reworked icon for adding todo.
    - Added a shutdown ImageButton that closes the application on click.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.gridlayout.widget.GridLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,48 +20,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Context context = getApplicationContext();
-
-        final ConstraintLayout constraintLayout = findViewById(R.id.rootContainer);
-        final Resolution resolution = new Resolution();
-//        final ImageView imageView = new ImageView(this);
-//        imageView.setLayoutParams(new ConstraintLayout.LayoutParams(resolution.width, resolution.height/2));
-        final List<ImageView> imageViews = new ArrayList<ImageView>();
+//
+//        final ConstraintLayout constraintLayout = findViewById(R.id.rootContainer);
+//        final Resolution resolution = new Resolution();
+////        final ImageView imageView = new ImageView(this);
+////        imageView.setLayoutParams(new ConstraintLayout.LayoutParams(resolution.width, resolution.height/2));
+//        final List<ImageView> imageViews = new ArrayList<ImageView>();
         final ImageButton addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final ImageView imageView = new ImageView(context);
-                imageViews.add(imageView);
-                ImageView currentView = imageViews.get(imageViews.size() - 1);
-                currentView.setLayoutParams(new ConstraintLayout.LayoutParams(300, (imageViews.size() * 300 + 300)));
-                currentView.setImageResource(R.drawable.user_to_do);
-                constraintLayout.addView(currentView);
-                currentView.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        System.out.println("Clicked on imageView " + v);
-                        EditText ed = new EditText(context);
-                        ed.setInputType(InputType.TYPE_CLASS_TEXT);
-                        ed.setLayoutParams(new ConstraintLayout.LayoutParams(600, (imageViews.size() * 300 + 300)));
-                        System.out.println("Theoretically something was added.");
-
-                    }
-                });
-                System.out.println("Adding to-do, size of list = " + imageViews.size());
+                final TextView t1 = new TextView(context);
+                t1.append("tsvis");
+                final GridLayout gridLayout = findViewById(R.id.gridLayout);
+                gridLayout.addView(t1);
+//                final ImageView imageView = new ImageView(context);
+//                imageViews.add(imageView);
+//                ImageView currentView = imageViews.get(imageViews.size() - 1);
+//                currentView.setLayoutParams(new ConstraintLayout.LayoutParams(300, (imageViews.size() * 300 + 300)));
+//                currentView.setImageResource(R.drawable.user_to_do);
+//                constraintLayout.addView(currentView);
+//                currentView.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        System.out.println("Clicked on imageView " + v);
+//                        EditText ed = new EditText(context);
+//                        ed.setInputType(InputType.TYPE_CLASS_TEXT);
+//                        ed.setLayoutParams(new ConstraintLayout.LayoutParams(600, (imageViews.size() * 300 + 300)));
+//                        System.out.println("Theoretically something was added.");
+//
+//                    }
+//                });
+//                System.out.println("Adding to-do, size of list = " + imageViews.size());
             }
         });
-        final Button rmButton = findViewById(R.id.rmButton);
-        rmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (imageViews.size() > 0) {
-                    int indexOfLastView = imageViews.size() - 1;
-                    imageViews.get(indexOfLastView).setVisibility(View.GONE);
-                    imageViews.remove(indexOfLastView);
-                }
-            }
-        });
-
+//        final Button rmButton = findViewById(R.id.rmButton);
+//        rmButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (imageViews.size() > 0) {
+//                    int indexOfLastView = imageViews.size() - 1;
+//                    imageViews.get(indexOfLastView).setVisibility(View.GONE);
+//                    imageViews.remove(indexOfLastView);
+//                }
+//            }
+//        });
+//
 
 //        ConstraintLayout constraintLayout = findViewById(R.id.rootContainer);
 //        if (constraintLayout != null) {
@@ -70,6 +75,13 @@ public class MainActivity extends AppCompatActivity {
 //            constraintLayout.addView(imageViews.get(0));
 //        }
 
+        final ImageButton shutdownButton = findViewById(R.id.shutdownButton);
+        shutdownButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAndRemoveTask();
+            }
+        });
 
     }
 
